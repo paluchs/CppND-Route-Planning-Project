bec985a6ae2417fc2f7ad67781bff037d742d38a
patrickluchsinger <patrick.lu@bluewin.ch>
2024-06-07 11:31:59 +0200

    Set the pointer to the start node and the end node

--- src/main.cpp
+++ src/main.cpp
@@ -62,16 +62,16 @@ int main(int argc, const char **argv)
     // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
-    float start_x = PromptCoordinate("longitude", "starting");
-    float start_y = PromptCoordinate("latitude", "starting");
-    float end_x = PromptCoordinate("longitude", "end");
-    float end_y = PromptCoordinate("latitude", "end");
+    float start_x = PromptCoordinate("x value", "starting");
+    float start_y = PromptCoordinate("y value", "starting");
+    float end_x = PromptCoordinate("x value", "end");
+    float end_y = PromptCoordinate("y value", "end");
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
